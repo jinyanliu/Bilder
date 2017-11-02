2db710389cf6ef6ef79951a129ab240f7a3724c3
jinyanliu <jane.jinyan.liu@gmail.com>
2017-11-02 16:35:16 +0100

    Small Change in MainActivity.

--- app/src/main/java/se/sugarest/jane/bilder/ui/MainActivity.java
+++ app/src/main/java/se/sugarest/jane/bilder/ui/MainActivity.java
@@ -151,7 +151,6 @@ public class MainActivity extends AppCompatActivity implements PhotoAdapter.Phot
     }
 
     private void setPhotoListDataToRecyclerView(List<Photo> photoLists) {
-        showRecyclerView();
         mPhotoUrlStrings = new ArrayList<>();
         for (int i = 0; i < photoLists.size(); i++) {
             Photo currentPhoto = photoLists.get(i);
@@ -164,6 +163,7 @@ public class MainActivity extends AppCompatActivity implements PhotoAdapter.Phot
             Log.i(LOG_TAG, "CurrentPhotoUrl = " + currentPhotoUrl);
             mPhotoUrlStrings.add(currentPhotoUrl);
         }
+        showRecyclerView();
         mPhotoAdapter.setPhotoData(mPhotoUrlStrings);
         Log.i(LOG_TAG, "Set photo date to adapter.");
     }
